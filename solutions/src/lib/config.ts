8652ba8abdc7591586add0f2d66c32571653aaa5
bryan leighton <bryan.leigh77@gmail.com>
2022-09-23 12:03:39 -0700

    use vite import

--- solutions/src/lib/config.ts
+++ solutions/src/lib/config.ts
@@ -1,9 +1,2 @@
-// export const BASE: string = import.meta.env.VITE_BASE
-// export const APP_ID: string = import.meta.env.VITE_APP_ID
-console.log(import.meta.env)
-// export const BASE = import.meta.env.VITE_BASE
-// export const APP_ID = import.meta.env.VITE_APP_ID
-
-
-export const BASE = "https://demo.playfab.tv"
-export const APP_ID = "v0u9yv"
+export const BASE = import.meta.env.VITE_BASE
+export const APP_ID = import.meta.env.VITE_APP_ID
