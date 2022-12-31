9f70e4c155d3a053c549456cf11ba24af760d5f1
Bryan Leighton <bryan.leigh77@gmail.com>
2022-12-31 16:16:05 +0100

    profile frontend

---
+++ solutions/src/lib/components/Badge.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { Card } from "flowbite-svelte";
+
+  export let badgeNum: number;
+</script>
+
+<Card class="badge" padding="none">
+  <div class="card-layout">
+    <img
+      style={badgeNum === 1 ? "opacity: 1" : "opacity: 0.2"}
+      class="rounded-t-sm"
+      src="/images/icons8-great-britain-96.png"
+      alt="great britain badge"
+    />
+  </div>
+</Card>
+
+<style>
+  .card-layout img {
+    height: 96px;
+  }
+
+  .card-layout {
+    height: 240px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
--- solutions/src/lib/components/BadgesList.svelte
+++ solutions/src/lib/components/BadgesList.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { Card, Button, Rating, Badge } from "flowbite-svelte";
+  import Badge from "$lib/components/Badge.svelte";
   const badgeCount = [1, 2, 3, 4, 5, 6];
 
 </script>
@@ -8,17 +8,7 @@
 
 <section class="badges-section">
   {#each badgeCount as badgeNum }
-    <Card class="badge" padding="none">
-      {#if badgeNum === 1}
-      <div class="card-layout">
-        <img style="opacity: 1" height=96 width=96 class="rounded-t-sm" src="/images/icons8-great-britain-96.png" alt="great britain badge" />
-      </div>
-      {:else}
-      <div class="card-layout">
-        <img height=96 width=96 class="rounded-t-sm" src="/images/icons8-great-britain-96.png" alt="great britain badge" />
-      </div>
-      {/if}
-    </Card>
+    <Badge badgeNum={badgeNum} />
   {/each}
 </section>
 
@@ -26,11 +16,8 @@
 <style>
   .badges-section {
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(240px, 1fr));
     gap: 1rem;
-    padding: 1rem;
-  }
-  .card-layout img {
-    opacity: 0.2;
+    margin-top: 1rem;
   }
 </style>
