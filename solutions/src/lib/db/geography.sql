fb308b6e668afba73c99b054b729ee9fd53ca135
Bryan Leighton <bryan.leigh77@gmail.com>
2023-02-17 17:42:51 -0800

    geo seed script

--- solutions/src/lib/db/geography.sql
+++ solutions/src/lib/db/geography.sql
@@ -1,7 +1,5 @@
-
 insert into public.courses (id, title, description, courseDate) values
-(3, 'Geography', 'Geography 101 on Physical and Human Geography', '2022-08-01T00:00:00.000+00:00'),
-
+(3, 'Geography', 'Geography 101 on Physical and Human Geography', '2022-08-01T00:00:00.000+00:00');
 
 insert into public.modules (id, title, description, course_id) values
 (6, 'Physical Geography', 'The study of the natural environment', 3),
